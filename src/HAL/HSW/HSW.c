af850fd417a37540424457d0e950aaae4ecc9b27
Fatma Ezzat <83879866+FatmaEzzat2@users.noreply.github.com>
2024-04-08 16:32:13 +0200

    Update HSW.c

--- src/HAL/HSW/HSW.c
+++ src/HAL/HSW/HSW.c
@@ -66,7 +66,8 @@ void HSW_Runnable(void){
 	}
 }
 
-SW_ErrorState SW_getSwState(SWs_t switchName,u8* switchState){
+
+SW_ErrorState SWITCH_enuGetStatus(SWs_t switchName,u8* switchState){
 	SW_ErrorState returnError = SW_enumNOK;
 	*switchState = swState[switchName];
 	return returnError;
